393915b16603e8a2362315bd5bfc372a3690ff4d
JoseGuadalupeML <meza.luna.jg@gamil.com>
2026-08-25 17:56:26 -0600

    feat: Sistema_Cafeteria

---
+++ backend/src/main/java/com/sedif/sistema_cafeteria/SistemaCafeteriaApplication.java
@@ -0,0 +1,13 @@
+package com.sedif.sistema_cafeteria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaCafeteriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaCafeteriaApplication.class, args);
+	}
+
+}
